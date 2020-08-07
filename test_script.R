e0d50c404b222e3a43b231fad4a947c9d2c87a28
Jamille Veiga <58223957+jamilleveiga@users.noreply.github.com>
2020-08-06 22:06:25 -0300

    Check changes in R

--- test_script.R
+++ test_script.R
@@ -5,5 +5,7 @@ test_function <- fucntion(){
   b <- sd()
   ### see this change in github!
   ### add any other variable you want
+  
+  ## Now adding a comment from github - check in R!
   return(result)
 }
